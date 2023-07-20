1926fa0bb14d4572285cafeebf0a656d97659e09
Oliver Erdmann <oliver@versa-networks.com>
2023-07-19 20:13:21 -0700

    init

---
+++ client/src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+/// Example of declaring env variables in TS below:
+
+
+// interface ImportMetaEnv {
+//     VITE_APP_TITLE: string;
+//     VITE_API_URL: string;
+//     // Add more environment variables here as needed
+//   }
+
+// ==> call them like this: const title: string = import.meta.env.VITE_APP_TITLE;
