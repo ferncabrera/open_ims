74f32c34c4206d2f547a15821198c92c7a9b6ebb
Oliver Erdmann <oliver@versa-networks.com>
2023-07-28 16:13:45 -0700

    apply global styling

--- microservices/client/src/main.tsx
+++ microservices/client/src/main.tsx
@@ -14,9 +14,12 @@ const router = createBrowserRouter(Routes, {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider
+      <div className='open-ims-styling'>
+      <RouterProvider
             router={router}
             fallbackElement={<><p>Spinner here lol</p></>}
         />
+      </div>
+        
     </React.StrictMode>,
 )
