8739552ac798ae40c5a52dd06321e3cea4df3fdf
Fernando Cabrera <ferncabrera01@outlook.com>
2023-08-04 19:08:45 -0400

    Merge conflicts

--- microservices/server/index.ts
+++ microservices/server/index.ts
@@ -1,13 +1,8 @@
 import express, { Express, Request, Response } from "express";
 import cors from 'cors';
 import morgan from 'morgan';
-<<<<<<< HEAD
-import * as db from './db/index'
-db.seed();
-=======
 import { initializeRoutes } from "./src/routes/initialize";
 import { seed } from "./src/db/index";
->>>>>>> 73027c5d3be292f9081c17e0660fc663ca3a4086
 
 const app: Express = express();
 
@@ -24,7 +19,4 @@ app.get('*', (req: Request, res: Response) => {
 
 app.listen(process.env.SERVER_PORT, () => { console.log('[server compiled]: Running on port ', process.env.SERVER_PORT, ` (http://localhost:${process.env.SERVER_PORT}/)`) });
 
-<<<<<<< HEAD
-=======
 seed();
->>>>>>> 73027c5d3be292f9081c17e0660fc663ca3a4086
