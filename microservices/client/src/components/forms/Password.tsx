98e06a35d06a553b52dbae647178cf17ff49530b
Oliver Erdmann <oliver@versa-networks.com>
2023-08-08 23:05:18 -0700

    Change icons in validations

--- microservices/client/src/components/forms/Password.tsx
+++ microservices/client/src/components/forms/Password.tsx
@@ -56,17 +56,19 @@ export const Password: React.FC<IPasswordProps> = (props) => {
 
                 </InputGroup>
                 {props.isInvalid &&
-                    <div data-testid="password-error" style={{ fontSize: "14px" }} className='invalid'>
-                        {props.errorMessage ? props.errorMessage :
-                            <>
-                                <p className='mb-0'>Password does not meet password requirements:</p>
-                                <ul>
-                                    <li>must be at least 8 - 64 characters long.</li>
-                                    <li>must include one lowercase letter, one uppercase letter, one number, and one special character.</li>
-                                </ul>
-                            </>
-                        }
-                    </div>
+                    <>
+                        <div data-testid="password-error" className='invalid'>
+                            {props.errorMessage ? <div className='invalid-feedback'>{props.errorMessage} </div> :
+                                <>
+                                    <p className='mb-0 invalid-feedback' style={{ fontSize: "14px" }}>Password does not meet password requirements:</p>
+                                    <ul className='invalid' style={{ fontSize: "14px" }}>
+                                        <li>must contain 8 to 64 characters.</li>
+                                        <li>must include one lowercase letter, one uppercase letter, one number, and one special character.</li>
+                                    </ul>
+                                </>
+                            }
+                        </div>
+                    </>
                 }
             </div>
         </>
