966938ad6a4e0c73ed893f6eb588b4da472c2cb4
JosephFanous <joseph_fanous@hotmail.com>
2023-08-15 01:28:14 -0400

    added more to forget password

--- microservices/server/src/controllers/forgot_pass.ts
+++ microservices/server/src/controllers/forgot_pass.ts
@@ -1,50 +1,70 @@
 // server call for forgot password
-import { Request, Response } from 'express';
+import { Request, Response } from "express";
 import { query } from "../db";
 import * as jwt from "jsonwebtoken";
 import * as bcrypt from "bcrypt";
-import customError from '../utils/customeError';
-
+import customError from "../utils/customeError";
+var Brevo = require("@getbrevo/brevo");
+var defaultClient = Brevo.ApiClient.instance;
 
 export const forgot_pass = async (req: Request, res: Response) => {
+  const { email } = req.body;
+  // console.log("this is the api call: ", email);
+  // res.json({"this is the api call: ": email})
 
-    const {email} = req.body;
-    // console.log("this is the api call: ", email);
-    // res.json({"this is the api call: ": email})
-    
-    // using the email, find the user in the database
-    const userEmail = await query('SELECT * FROM user_table WHERE email = $1', [email]);
+  // using the email, find the user in the database
+  const userEmail = await query("SELECT * FROM user_table WHERE email = $1", [
+    email,
+  ]);
 
-    const user = userEmail.rows[0];
-    if (!user) {
-        throw new customError({message: "Email does not exist", code:10});
+  const user = userEmail.rows[0];
+  if (!user) {
+    throw new customError({ message: "Email does not exist", code: 10 });
+  }
+
+  // const secret = process.env.JWT_SECRET_TOKEN;
+  // const expirationDate = Math.floor(Date.now() / 1000) + 60 * 10; // expiration is 10 minutes
+  // const token = jwt.sign(
+  //   { exp: expirationDate, email: user.email, permission: user.permission },
+  //   `${secret}`
+  // );
+  // res.cookie("authToken", token, { httpOnly: true, secure: true });
+
+  // Configure API key authorization: api-key
+  var apiKey = defaultClient.authentications["api-key"];
+  apiKey.apiKey =
+    "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";
+  // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+  // apiKey.apiKeyPrefix["api-key"] = "Token";
+
+  // Configure API key authorization: partner-key
+  // var partnerKey = defaultClient.authentications["partner-key"];
+  // partnerKey.apiKey = "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";
+  // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
+  // partnerKey.apiKeyPrefix["partner-key"] = "Token";
+
+  // var api = new Brevo.AccountApi();
+
+  var apiInstance = new Brevo.TransactionalEmailsApi();
+
+  var sendSmtpEmail = new Brevo.SendSmtpEmail(); // SendSmtpEmail | Values to send a transactional email
+
+  apiInstance.sendTransacEmail(sendSmtpEmail).then(
+    function (data: any) {
+      console.log("API called successfully. Returned data: " + data);
+    },
+    function (error: any) {
+      console.error(error);
     }
-
-
-
-    // if the user does not exist, return an error
-/*
-    // this format makes it less susceptible to SQL injection (directly injecting into the query)
-    const user = userEmail.rows[0];
-    if (!user) {
-        throw new customError({message: "Email or Password is Incorrect", code:10});
-    }
-
-    const secret = process.env.JWT_SECRET_TOKEN;
-    const expirationDate = Math.floor(Date.now() / 1000) + (60 * 5) // expiration is 5 minutes
-    const token = jwt.sign({exp: expirationDate, email: user.email, permission: user.permission}, `${secret}`);
-    res.cookie('authToken', token, {httpOnly: true, secure: true});
-    res.json({message:"Success"})
-    */
-    // if (user.rows.length === 0) {
-    //     return res.status(404).json('User not found');
-    // } else {
-    //     return res.status(200).json('User found');
-    // }
-    // if the user exists, send an email with a link to reset the password
-    // the link should include a token that will expire after 1 hour
-    // the token should be saved in the database
-    // the token should be deleted after the password is reset
-
-
+  );
+  
+  // api.getAccount().then(
+  //   function (data: any) {
+  //     res.json({ message: "Success" + data. });
+  //     // console.log("API called successfully. Returned data: " + data);
+  //   },
+  //   function (error: any) {
+  //     console.error(error);
+  //   }
+  // );
 };
