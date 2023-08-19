0bb5be08af57ad1a8d365b7b76605ee7855d4b25
Oliver Erdmann <oliver@versa-networks.com>
2023-08-19 13:32:28 -0700

    Implement profile Icon on sidebar

--- microservices/client/src/components/mainComponents/ProfileIcon.tsx
+++ microservices/client/src/components/mainComponents/ProfileIcon.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import { MdNotificationsNone } from 'react-icons/md';
 
 import styles from "./profileIcon.module.scss";
 
 export const ProfileIcon = () => {
-  return (
-    <>
-        <div className={styles.profileImage}>
+    return (
+        <>
+                <div className={styles.circleContainer}>
 
-        </div>
-    </>
-  )
+                    <div className={styles.profileImage}>
+                        <div className={styles.notificationIcon}>
+                            <MdNotificationsNone />
+                        </div>
+
+                    </div>
+                </div>
+        </>
+    )
 }
--- microservices/client/src/components/mainComponents/Sidebar.tsx
+++ microservices/client/src/components/mainComponents/Sidebar.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { Nav, Image } from 'react-bootstrap';
+import { Nav, Image, Row, Col } from 'react-bootstrap';
 import logoWithText from "../../assets/public/svgs/logoWithText.svg"
 import {
-    MdOutlineHome, 
-    MdOutlineFace, 
-    MdOutlineSell, 
+    MdOutlineHome,
+    MdOutlineFace,
+    MdOutlineSell,
     MdOutlineStorefront,
     MdOutlineLocalMall,
     MdOutlineCategory,
     MdOutlineSupervisedUserCircle
 } from "react-icons/md"
+import { ProfileIcon } from './ProfileIcon';
 
 import styles from "./sidebar.module.scss";
 
@@ -25,31 +26,42 @@ export const Sidebar = () => {
                     <Image className={styles.logo} src={logoWithText} />
                 </div>
                 <Nav.Item>
-                    <Nav.Link href="/ccg/dashboard"><span><MdOutlineHome/></span>Dashboard</Nav.Link>
+                    <Nav.Link href="/ccg/dashboard"><span><MdOutlineHome /></span>Dashboard</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="customers"><span><MdOutlineFace/></span>Customers</Nav.Link>
+                    <Nav.Link eventKey="customers"><span><MdOutlineFace /></span>Customers</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="salesOrders"><span><MdOutlineSell/></span>Sales Orders</Nav.Link>
+                    <Nav.Link eventKey="salesOrders"><span><MdOutlineSell /></span>Sales Orders</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="vendors"><span><MdOutlineStorefront/></span>Vendors</Nav.Link>
+                    <Nav.Link eventKey="vendors"><span><MdOutlineStorefront /></span>Vendors</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="purchaseOrders"><span><MdOutlineLocalMall/></span>Purchase Orders</Nav.Link>
+                    <Nav.Link eventKey="purchaseOrders"><span><MdOutlineLocalMall /></span>Purchase Orders</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="products"><span><MdOutlineCategory/></span>Products</Nav.Link>
+                    <Nav.Link eventKey="products"><span><MdOutlineCategory /></span>Products</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="users"><span><MdOutlineSupervisedUserCircle/></span>Users</Nav.Link>
+                    <Nav.Link eventKey="users"><span><MdOutlineSupervisedUserCircle /></span>Users</Nav.Link>
                 </Nav.Item>
 
-                <div className='position-absolute bottom-0 ms-3 mb-4'>
-                    adlibs
+                <div className={styles.profileIcon}>
+                    <ProfileIcon />
+                    <div className='d-flex flex-column'>
+                        <div>
+
+                            <strong>{"[name]"}</strong>
+                        </div>
+                        <div>
+                            {"[permission]"}
+                        </div>
+                    </div>
+
+
                 </div>
-                
+
             </Nav>
 
         </>
