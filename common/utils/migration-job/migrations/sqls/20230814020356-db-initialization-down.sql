4bb4c66161b3365c1f7ba4ad7305e428eabfab94
Kelly Gibson <kellygibson@Kellys-MacBook-Pro-2.local>
2023-08-20 19:14:14 -0400

    merging

--- common/utils/migration-job/migrations/sqls/20230814020356-db-initialization-down.sql
+++ common/utils/migration-job/migrations/sqls/20230814020356-db-initialization-down.sql
@@ -4,3 +4,12 @@ DROP TABLE IF EXISTS public.user_table;
 
 DROP TYPE IF EXISTS public.user_permission;
 
+DROP TABLE IF EXISTS public.customer_table;
+
+DROP TABLE IF EXISTS public.vendor_table;
+
+DROP TABLE IF EXISTS public.customer_addresses;
+
+DROP TABLE IF EXISTS public.vendor_addresses;
+
+DROP TYPE IF EXISTS public.address_type;
--- common/utils/migration-job/migrations/sqls/20230814020356-db-initialization-up.sql
+++ common/utils/migration-job/migrations/sqls/20230814020356-db-initialization-up.sql
@@ -1,5 +1,6 @@
 /* Replace with your SQL commands */
 
+-- USER TABLE 
 DO $$ BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_permission') THEN
         CREATE TYPE user_permission AS ENUM ('Admin', 'Employee', 'Customer', 'Vendor', 'Unauthorized');
@@ -14,4 +15,62 @@ CREATE TABLE IF NOT EXISTS user_table (
     phone VARCHAR(50),
     permission user_permission DEFAULT 'Unauthorized' NOT NULL,
     password VARCHAR(128) NOT NULL CHECK (Password ~ '^.*(?=.{8,64})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).*$')
-)
+);
+
+ -- CUSTOMER TABLE
+CREATE TABLE IF NOT EXISTS customer_table (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    company_name VARCHAR(50) NOT NULL,
+    phone VARCHAR(15) NOT NULL
+);
+
+-- VENDOR TABLE
+CREATE TABLE IF NOT EXISTS vendor_table (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    company_name VARCHAR(50) NOT NULL,
+    phone VARCHAR(15) NOT NULL 
+);
+
+-- CUSTOMER AND VENDOR ADDRESSES
+DO $$ BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'address_type') THEN
+        CREATE TYPE address_type AS ENUM ('Shipping', 'Billing');
+    END IF;
+END $$;
+
+CREATE TABLE IF NOT EXISTS customer_addresses (
+    id SERIAL PRIMARY KEY,
+    customer_id INT NOT NULL,
+    address address_type NOT NULL,
+    street_address_line1 VARCHAR(100) NOT NULL,
+    street_address_line2 VARCHAR(100),
+    city VARCHAR(35) NOT NULL,
+    province VARCHAR(35),
+    postal VARCHAR(12) NOT NULL,
+    country VARCHAR(35) NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customer_table(id) ON DELETE CASCADE
+);
+
+-- CREATE TABLE IF NOT EXISTS vendor_addresses (
+--     id SERIAL PRIMARY KEY,
+--     vendor_id NOT NULL,
+--     address address_type NOT NULL,
+--     street_address_line1 VARCHAR(100) NOT NULL,
+--     street_address_line2 VARCHAR(100),
+--     city VARCHAR(35) NOT NULL,
+--     province VARCHAR(35),
+--     postal VARCHAR(12) NOT NULL,
+--     country VARCHAR(35) NOT NULL,
+--     FOREIGN KEY (vendor_id) REFERENCES vendor_table(id) ON DELETE CASCADE
+-- );
+
+
+
+
+
