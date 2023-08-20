07d6b29a7fb42c0b520ca9ec5658fbcb304b4cc0
Fernando Cabrera <ferncabrera01@outlook.com>
2023-08-20 10:44:21 -0400

    cleaning code

--- microservices/client/src/utilities/apiHelpers.ts
+++ microservices/client/src/utilities/apiHelpers.ts
@@ -1,5 +1,5 @@
 import { apiCall } from "./apiCall";
-import { IGetResponse, IPostRequest } from "./types/types"
+import { IGetResponse, IPostRequest } from "./types/types";
 
 export const getJSONResponse = async (props: IGetResponse) => {
     const
@@ -11,13 +11,6 @@ export const getJSONResponse = async (props: IGetResponse) => {
     console.log(import.meta.env.MODE);
     console.log(import.meta.env.VITE_HOST_PROD);
     console.log(import.meta.env.VITE_HOST_DEV);
-    // console.log("import.meta\u200b.env.VITE_HOST_DEV -> ", 'import.meta\u200b.env.VITE_HOST_DEV');
-    // console.log("import.meta\u200b.env.VITE_HOST_DEV -> ", import.meta.env.VITE_HOST_PROD);
-
-    // console.log("--------------------------")
-    // console.log(`import.meta.env.MODE -> ${import.meta.env.MODE}`);
-    // console.log(`import.meta.env.VITE_HOST_PROD -> ${import.meta.env.VITE_HOST_PROD}`);
-    // console.log(`import.meta.env.VITE_HOST_DEV -> ${import.meta.env.VITE_HOST_DEV}`);
 
     const url = `http://${import.meta.env.DEV?import.meta.env.VITE_HOST_DEV:import.meta.env.VITE_HOST_PROD}${endpoint}`;
     const parameters = params ? params : {}; //can add default headers here in the future.
