9fd72b8cce2a7be622a69c17cf59feee82f6c6b7
Oliver Erdmann <oliver@versa-networks.com>
2023-11-05 08:09:10 -0500

    mock test folder structure change

--- microservices/client/vite.config.ts
+++ microservices/client/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./tests/setup.js', './src/mocks/setupMocks.ts'],
+    setupFiles: ['./tests/setup.js', './tests/mocks/setupMocks.ts'],
     coverage: {
       provider: 'v8',
     }
