fc5ddfd879973e36bc6259c970325d27c7608bf8
Polsander <oerdmann@uwaterloo.ca>
2024-01-11 23:52:33 -0500

    fix import error

--- microservices/server/src/routes/vendors.ts
+++ microservices/server/src/routes/vendors.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import catchAsync from "../utils/catchAsync";
-import errorHandler from "../../middleware/errorHandler";
+import errorHandler from "../middleware/errorHandler";
 import { get_all_vendors, get_available_vendors } from "../controllers/vendors";
 
 const router = express.Router();
