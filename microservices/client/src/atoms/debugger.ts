0a6f17e479238f24804203c217fd04e8b27f1e70
Polsander <oerdmann@uwaterloo.ca>
2024-06-16 14:05:02 +0200

    Remove deprecated comment

--- microservices/client/src/atoms/debugger.ts
+++ microservices/client/src/atoms/debugger.ts
@@ -1,18 +1,3 @@
-//! DEPRECATED
-// import React, {useEffect} from "react";
-// import { useRecoilSnapshot } from "recoil";
-
-// export function DebugObserver() {
-//     const snapshot = useRecoilSnapshot();
-//     useEffect(() => {
-//       console.log('The following atoms were modified:');
-//       for (const node of snapshot.getNodes_UNSTABLE({isModified: true})) {
-//         console.log(node.key, snapshot.getLoadable(node));
-//       }
-//     }, [snapshot]);
-  
-//     return null;
-//   }
 import { useAtomsDevtools } from 'jotai-devtools/utils';
 
 export const DebugAtoms = () => {
